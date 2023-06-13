141b12a816a3405030479d088d3454e573224fee
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-06-13 15:38:12 +0300

    project updated/input file ready

--- admin-form/src/input.js
+++ admin-form/src/input.js
@@ -21,41 +21,59 @@ function Input() {
   return (
     <>
       <div className={styles.emailContainer}>
-        <h2>Let's stay connected.</h2>
+        <h2>Hello. Welcome our school</h2>
         <p>
-          If you would like to stay up-to-date with our latest products, connect
-          with other students who share your interests and passion 
-          and receive unique offers, please sign up for our newsletter 
+          Please feel in the following information so that we can get to know you better. 
         </p>
         <form>
         <label for='firstName'>First Name: </label>
         <input id='firstName' type='text' onChange={handleInputChange} value={formData.firstName} />
-
+        <br />
         <label for='lastName'>Last Name: </label>
         <input id='lastName' type='text' onChange={handleInputChange} value={formData.lastName} />
-
+        <br />
         <label for='age'>Age: </label>
         <input id='age' type='text' onChange={handleInputChange} value={formData.age} />
-
+        <br />
         <label for='email'>email: </label>
         <input id='email' type='text' onChange={handleInputChange} value={formData.email} />
-
+        <br />
         <label for='address'>Address: </label>
         <input id='address' type='text' onChange={handleInputChange} value={formData.address} />
-
+        <br />
         <label for='homeRoomClassNumber'>Home Room Class Number: </label>
         <input id='homeRoomClassNumber' type='text' onChange={handleInputChange} value={formData.homeRoomClassNumber} />
-
+        <br />
         <label for='studentId'>Student ID: </label>
         <input id='studentId' type='text' onChange={handleInputChange} value={formData.studentId} />
+        <br />
         
-        <label for='lastName'>Last Name: </label>
-        <input id='lastName' type='text' onChange={handleInputChange} value={formData.lastName} />
+        <p>Favorite Lunch:</p>
+
+        <input type='radio' id='ice-cream' value='ice-cream' name='favoriteLunch' onChange={handleInputChange} checked={formData.favoriteLunch === 'ice-cream'} />
+        <label for='ice-cream'>Icecream</label><br />
+        <input type='radio' id='pizza' value='pizza' name='favoriteLunch' onChange={handleInputChange} checked={formData.favoriteLunch === 'pizza'} />
+        <label for='pizza'>Pizza</label><br />
+        <input type='radio' id='salmon' value='salmon' name='favoriteLunch' onChange={handleInputChange} checked={formData.favoriteLunch === 'salmon'} />
+        <label for='salmon'>Salmon</label><br />
+        <input type='radio' id='salad' value='salad' name='favoriteLunch' onChange={handleInputChange} checked={formData.favoriteLunch === 'salad'} />
+        <label for='salad'>Salad</label><br />
+        <input type='radio' id='pasta' value='pasta' name='favoriteLunch' onChange={handleInputChange} checked={formData.favoriteLunch === 'pasta'} />
+        <label for='pasta'>Pasta</label><br />
+        <input type='radio' id='burger' value='burger' name='favoriteLunch' onChange={handleInputChange} checked={formData.favoriteLunch === 'burger'} />
+        <label for='burger'>Burger</label><br />
         </form>
       </div>
       <div className={styles.inputDisplay}>
         <h2>Current User Input: </h2>
         <h4>{formData.firstName}</h4>
+        <h4>{formData.lastName}</h4>
+        <h4>{formData.age}</h4>
+        <h4>{formData.email}</h4>
+        <h4>{formData.address}</h4>
+        <h4>{formData.homeRoomClassNumber}</h4>
+        <h4>{formData.studentId}</h4>
+        <h4>{formData.favoriteLunch}</h4>
       </div>
 
     </>
