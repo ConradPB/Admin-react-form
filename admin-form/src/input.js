90ec74b107d55187bf81c016aacdfeed5430164a
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-08-22 15:24:36 +0300

    updated app/project complete

--- admin-form/src/input.js
+++ admin-form/src/input.js
@@ -25,101 +25,100 @@ function Input() {
         <p>
           Please feel in the following information so that we can get to know you better. 
         </p>
-        <div className={`${styles.formContainer}`}>
-          
-          <form>
-          <h3>Personal Information</h3>
+        
+        <form>
+        <h3>Personal Information</h3>
+        <div className={styles.formRow}>
+          <div className={styles.formColumn}>
+            <label htmlFor='firstName'>First Name: </label>
+            <input id='firstName' name='firstName' type='text' onChange={handleInputChange} value={formData.firstName} />
+            </div> 
+            <br />
+        <div className={styles.formColumn}>
+            <label htmlFor='lastName'>Last Name: </label>
+            <input id='lastName' name='lastName' type='text' onChange={handleInputChange} value={formData.lastName} />
+            </div>
+            
+        </div>
+          <br />
+        
+
+        <div className={styles.formRow}>
+            <div className={styles.formColumn}>
+              <label htmlFor='age'>Age: </label>
+              <input id='age' name='age' type='text' onChange={handleInputChange} value={formData.age} />
+            </div>
+            <br />
+        <div className={styles.formColumn}>
+            <label htmlFor='email'>Email: </label>
+            <input id='email' name='email' type='text' onChange={handleInputChange} value={formData.email} />
+            </div>
+        </div>
+        <h3>School Information</h3>
           <div className={styles.formRow}>
             <div className={styles.formColumn}>
-              <label htmlFor='firstName'>First Name: </label>
-              <input id='firstName' name='firstName' type='text' onChange={handleInputChange} value={formData.firstName} />
-              </div>
+              <label htmlFor='address'>Address: </label>
+              <input id='address' name='address' type='text' onChange={handleInputChange} value={formData.address} />
+              
+            </div>
+            <div className={styles.formColumn}>
               <br />
-          <div className={styles.formColumn}>
-              <label htmlFor='lastName'>Last Name: </label>
-              <input id='lastName' name='lastName' type='text' onChange={handleInputChange} value={formData.lastName} />
-              </div>
-          
+              <label htmlFor='homeRoomClassNumber'>Home Room Class Number: </label>
+              <input id='homeRoomClassNumber' name='homeRoomClassNumber' type='text' onChange={handleInputChange} value={formData.homeRoomClassNumber} />
+            </div>
           </div>
-            <br />
-          
-          <div className={styles.formRow}>
-              <div className={styles.formColumn}>
-                <label htmlFor='age'>Age: </label>
-                <input id='age' name='age' type='text' onChange={handleInputChange} value={formData.age} />
-              </div>
-              <br />
-          <div className={styles.formColumn}>
-              <label htmlFor='email'>Email: </label>
-              <input id='email' name='email' type='text' onChange={handleInputChange} value={formData.email} />
-              </div>
-          </div>
-          <h3>School Information</h3>
-            <div className={styles.formRow}>
-              <div className={styles.formColumn}>
-                <label htmlFor='address'>Address: </label>
-                <input id='address' name='address' type='text' onChange={handleInputChange} value={formData.address} />
-          
-              </div>
-              <div className={styles.formColumn}>
-                <br />
-                <label htmlFor='homeRoomClassNumber'>Home Room Class Number: </label>
-                <input id='homeRoomClassNumber' name='homeRoomClassNumber' type='text' onChange={handleInputChange} value={formData.homeRoomClassNumber} />
-              </div>
-            </div>
-          
-          </form>
-          <form className={`${styles.favoriteLunchForm}`}>
-            <div style={{ marginBottom: '5px' }}><h3>Favorite Lunch</h3>
-            <div style={{ marginTop: '10px' }}><p>What is your favorite lunch option?</p></div>
-            <div style={{ marginTop: '20px' }}>
-              <label>
-                <input
-          type="radio"
-          name="favoriteLunch"
-          value="pizza"
-          onChange={handleInputChange}
-          checked={formData.favoriteLunch === "pizza"}
-                />
-                Pizza
-              </label>
-              <br />
-              <label>
-                <input
-          type="radio"
-          name="favoriteLunch"
-          value="tacos"
-          onChange={handleInputChange}
-          checked={formData.favoriteLunch === "tacos"}
-                />
-                Tacos
-              </label>
-              <br />
-              <label>
-                <input
-          type="radio"
-          name="favoriteLunch"
-          value="sandwich"
-          onChange={handleInputChange}
-          checked={formData.favoriteLunch === "sandwich"}
-                />
-                Sandwich
-              </label>
-              <br />
-              <label>
-                <input
-          type="radio"
-          name="favoriteLunch"
-          value="salad"
-          onChange={handleInputChange}
-          checked={formData.favoriteLunch === "salad"}
-                />
-                Salad
-              </label>
-            </div>
-            </div>
-          </form>
-        </div>
+        
+        </form>
+        <form className={`${styles.favoriteLunchForm}`}>
+  <div><h3>Favorite Lunch</h3>
+  <p>What is your favorite lunch option?</p>
+  
+    <label>
+      <input
+        type="radio"
+        name="favoriteLunch"
+        value="pizza"
+        onChange={handleInputChange}
+        checked={formData.favoriteLunch === "pizza"}
+      />
+      Pizza
+    </label>
+    <br />
+    <label>
+      <input
+        type="radio"
+        name="favoriteLunch"
+        value="tacos"
+        onChange={handleInputChange}
+        checked={formData.favoriteLunch === "tacos"}
+      />
+      Tacos
+    </label>
+    <br />
+    <label>
+      <input
+        type="radio"
+        name="favoriteLunch"
+        value="sandwich"
+        onChange={handleInputChange}
+        checked={formData.favoriteLunch === "sandwich"}
+      />
+      Sandwich
+    </label>
+    <br />
+    <label>
+      <input
+        type="radio"
+        name="favoriteLunch"
+        value="salad"
+        onChange={handleInputChange}
+        checked={formData.favoriteLunch === "salad"}
+      />
+      Salad
+    </label>
+  
+  </div>
+</form>
 
 
 <div className={`${styles.inputDisplay}`}>
